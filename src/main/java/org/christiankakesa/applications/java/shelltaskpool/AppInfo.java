effdbf67293918c097123fb6ab49a1b55eadd3c6
Christian Kakesa <christian.kakesa@gmail.com>
2012-01-02 01:02:23 +0100

    Add application information class (plain hold class).

---
+++ src/main/java/org/christiankakesa/applications/java/shelltaskpool/AppInfo.java
@@ -0,0 +1,11 @@
+package org.christiankakesa.applications.java.shelltaskpool;
+
+import java.util.Calendar;
+
+public final class AppInfo {
+	public static final String APP_NAME = "shelltaskpool.jar";
+	public static final String AUTHOR_NAME = "Christian Kakesa";
+	public static final String AUTHOR_EMAIL = "christian.kakesa@gmail.com";
+	public static final String APP_COPYRIGHT = "Christian Kakesa (c) "
+			+ Calendar.getInstance().get(Calendar.YEAR);
+}
